540296453af89841aebc3f29c2266e3c11a8f8af
carlos <padron.ca@gmail.com>
2025-03-30 17:08:01 +0100

    Add comment to clarify data source limitation in get_data.py

--- snowflake/get_data.py
+++ snowflake/get_data.py
@@ -4,6 +4,7 @@ from snowflake.snowpark import Session
 #%%
 session = Session.builder.config("connection_name", "zpnlrxp-gn04931").create()
 # %%
+# cannot read from url, onlu from stage
 session.read.xml('https://planet.openstreetmap.org/planet/planet-latest.osm.bz2')
 
 #%%
