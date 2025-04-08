73926250aa48684df99e37777166bd2b772050a4
carlos <padron.ca@gmail.com>
2025-04-08 22:00:13 +0100

    Refactor data retrieval scripts to use AWS CLI for downloading OSM files and add installation script for AWS CLI

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,2 +1,5 @@
-wget https://planet.openstreetmap.org/planet/planet-latest.osm.bz2
-bzip2 -d planet-latest.osm.bz2
+# Download the OSM file
+aws s3 cp --no-sign-request s3://osm-planet-eu-central-1/planet/pbf/2025/planet-250331.osm.pbf .
+
+# Extract only buildings and save them to a CSV file
+ogr2ogr -f "CSV" buildings.csv planet-250331.osm.pbf multipolygons -where "building IS NOT NULL"
