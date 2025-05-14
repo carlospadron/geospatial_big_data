5f6dcd20b21e9ebe3a0da3ba850073c77fa22860
carlos <padron.ca@gmail.com>
2025-05-14 16:20:07 +0100

    using env vars

--- data/snowflake/get_data_into_s3.sh
+++ data/snowflake/get_data_into_s3.sh
@@ -6,7 +6,7 @@ echo "Script started at: $current_time"
 
 # Download the OSM file to own bucket
 aws s3 cp --no-sign-request s3://osm-pds/planet/planet-latest.orc . 
-aws s3 cp planet-latest.orc s3://giscarlos/osm/    
+aws s3 cp planet-latest.orc s3://$private_s3_bucket
 
 # Final Timestamp
 current_time2=$(date +"%Y-%m-%d %H:%M:%S")
